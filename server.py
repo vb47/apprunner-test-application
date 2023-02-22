0c3a03984a559053c0a9198b1f60dfa623350a6a
Vaibhav Alur <64318916+vb47@users.noreply.github.com>
2023-02-22 23:30:40 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,43 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
-import requests
-from bs4 import BeautifulSoup
-import os
 
-def hello_world(request):
-    name = os.environ.get('NAME')
-    if name == None or len(name) == 0:
-        name = "world"
-    message = "Hello, " + name + "!\n"
-    return Response(message)
+
+
 
 def get_data(request):
-    # the target we want to open	
-	url='https://www.hindustantimes.com/'
-	
-	#open with GET method
-	resp=requests.get(url)
-	
-	#http_respone 200 means OK status
-	if resp.status_code==200:
-		print("Successfully opened the web page")
-		print("The news are as follow :-\n")
-	
-		# we need a parser,Python built-in HTML parser is enough .
-		soup=BeautifulSoup(resp.text,'html.parser')	
+    resp = requests.get("https://news.ycombinator.com/")
 
-		# l is the list which contains all the text i.e news
-		l=soup.find("h3",{"class":"hdg3"})
-	
-		#now we want to print only the text part of the anchor.
-		#find all the elements of a, i.e anchor
-		str = ""
-		for i in l.findAll("a"):
-			str = str + i.text + " "
-		return Response(str)
-	else:
-		return Response("Error: " + str(resp.status_code))
+    txt = ""
+
+    if resp.status_code != 200:
+        print("Error fetching page")
+        exit()
+    else:
+        soup=BeautifulSoup(resp.text,'html.parser')
+        data = soup.find_all("span", {"class": "titleline"})
+
+        for i in data[0:5]:
+            txt = txt + i.text + "\n"
+    
+    return Response(txt)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT"))
